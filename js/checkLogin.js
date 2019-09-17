e98317cb1237f1958662d364aa9709ee0c840ef0
whitehox <oketegah@gmail.com>
2019-09-17 12:46:01 +0100

    check login

---
+++ js/checkLogin.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    let user = window.localStorage.getItem('email');
+    if(user){
+        $('#checkLog').html('You are logged in');
+    }else{
+        $('#checkLog').html('Please log in');
+        window.location.assign('register.html');
+    }
+});
